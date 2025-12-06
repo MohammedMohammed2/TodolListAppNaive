e763ea98bb55b76a5d9c6a3a80fc0d11245092b1
MohammedMohammed2 <mohammadxd100@gmail.com>
2025-12-06 21:51:00 +0100

    fixed issues

---
+++ app/tabs/settings.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SettngsScreen = () => {
+  return (
+    <View>
+      <Text>settngs</Text>
+    </View>
+  )
+}
+
+export default SettngsScreen
