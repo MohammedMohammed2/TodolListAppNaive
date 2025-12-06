885fd100aa5a7f3e5ae846b06e748148be1c08f7
MohammedMohammed2 <mohammadxd100@gmail.com>
2025-12-06 21:36:04 +0100

    new react native app

---
+++ app/tabs/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Edit app/index.tsx to edit this screens222.</Text>
+      <Text>be wary of nigga</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
